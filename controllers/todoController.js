1fd5d8dec275a1e42ecebffb5a8ef7e47f1c76b9
wahyu <komang01966@gmail.com>
2022-11-19 21:02:18 +0800

    Adding edit and delete method on todo

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,22 +1,37 @@
-import { PrismaClient } from "@prisma/client"
-const prisma = new PrismaClient()
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 class TodoController {
-    static async getTodo (req, res) {
-        const todoData = await prisma.todo.findMany()
-        res.status(200).json(todoData)
-    }
+  static async getTodo(req, res) {
+    const todoData = await prisma.todo.findMany();
+    res.status(200).json(todoData);
+  }
 
-    static async addTodo (req, res) {
-        const {title, desc} = req.body
-        const result = await prisma.todo.create({
-            data: {
-                title,
-                desc
-            }
-        })
-        res.status(201).json(result)
-    }
+  static async addTodo(req, res) {
+    const { title, desc } = req.body;
+    const result = await prisma.todo.create({
+      data: {
+        title,
+        desc,
+      },
+    });
+    res.status(201).json(result);
+  }
+
+  static async editTodo(req, res) {
+    const id = req.params.id;
+    const { title, desc } = req.body;
+    const result = await prisma.todo.update({
+      where: { id: id },
+      data: { title, desc },
+    });
+    res.status(200).json(result);
+  }
+
+  static async deleteTodo(req, res) {
+    const result = await prisma.todo.delete({ where: { id: req.params.id } });
+    res.status(200).json("Data deleted successfully");
+  }
 }
 
-export default TodoController
+export default TodoController;
--- routes/todo.js
+++ routes/todo.js
@@ -1,9 +1,11 @@
-import {Router} from "express"
-import TodoController from "../controllers/todoController.js"
+import { Router } from "express";
+import TodoController from "../controllers/todoController.js";
 
-const router = Router()
+const router = Router();
 
-router.get("/todo", TodoController.getTodo)
-router.post("/todo", TodoController.addTodo)
+router.get("/todo", TodoController.getTodo);
+router.post("/todo", TodoController.addTodo);
+router.put("/todo/:id", TodoController.editTodo);
+router.delete("/todo/:id", TodoController.deleteTodo);
 
-export default router
+export default router;
